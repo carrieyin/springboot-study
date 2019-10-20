f8ac9dd246fcd5bee328140136b0f655cca02715
yindd <695685009@qq.com>
2019-10-20 17:26:20 +0800

    解决非静态页面元素td绑定单击事件无反应（on问题）

--- pages/day1/index.js
+++ pages/day1/index.js
@@ -6,6 +6,15 @@ $(function () {
     $("#select").click(function(){
         userUserCrud.addUser("/apis/UserController/select")
     });
+    console.info('------------------',$("#del"))
+    $("#usertable tbody").on("click",".del",function () {
+        // var tr = $(this).parents("tr") ;
+        // var id = tr.find(":input[name='id']").val() ;
+        // console.info('id : ' + id) ;
+        var id =  $(this).attr("data-id") ;
+        console.info(id)
+        //userUserCrud.delUser("/apis/UserController/del");
+    });
 })
 
 function UserCRUD() {}
@@ -23,7 +32,10 @@ UserCRUD.prototype.getParam = function () {
 
 UserCRUD.prototype.appendTable = function (tableBody,resp) {
     for(var item of resp.data){
-        tableBody.append("<tr><td>"+ item.name +"</td><td>"+ item.addr +"</td></tr>");
+        console.info('----- > ',item)
+        var tr = $("<tr ><td><input type='hidden' value='"+item.id+"' name='id' /> "+ item.name
+            +" </td><td>"+ item.addr +"</td><td><a href='#' class='del' data-id ='"+item.id+"' >删除</a>&nbsp;&nbsp;&nbsp;<a  href='#' >修改</a></td></tr>") ;
+        tableBody.append(tr);
     }
 }
 
@@ -36,4 +48,14 @@ UserCRUD.prototype.addUser = function (url) {
     $.when(ajaxing).done(function (resp) {
         _slef.appendTable(tableBody, resp);
     });
+}
+
+UserCRUD.prototype.delUser = function (url) {
+    var userid = this.siblings("#userid");
+    console.log(userid);
+    var _slef = this;
+    // var ajaxing = httpUtil.dealAjaxRequest4JSObj(url, param);
+    // $.when(ajaxing).done(function (resp) {
+    //     _slef.appendTable(tableBody, resp);
+    // });
 }
--- src/main/java/com/ydd/boot/controller/IndexController.java
+++ src/main/java/com/ydd/boot/controller/IndexController.java
@@ -3,7 +3,6 @@ package com.ydd.boot.controller;
 import com.ydd.boot.common.ResponVo;
 import com.ydd.boot.model.User;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -13,17 +12,43 @@ import java.util.List;
 @RequestMapping("/apis/UserController")
 @Slf4j
 public class IndexController {
-    private List<User> userList = new ArrayList();
+    private List<User> userList = new ArrayList<>();
     private Integer id = 0;
-    private List findUser(User user){
-        List<User> listUser = new ArrayList();
+    private List<User> filterUserByName(List<User> userList, String name){
+        if("".equals(name)){
+            return userList;
+        }
+        List<User> listUser = new ArrayList<>();
         for(User userinfo: userList){
-            if(user.getName() == userinfo.getName() && user.getAddr() == userinfo.getAddr()){
+            if(userinfo.getName().contains(name)){
                 listUser.add(userinfo);
             }
         }
         return listUser;
     }
+    private List<User> filterUserByAddr(List<User> userList, String addr){
+        if("".equals(addr)){
+            return userList;
+        }
+        List<User> listUser = new ArrayList<>();
+        for(User userinfo: userList){
+            if(userinfo.getAddr().contains(addr)){
+                listUser.add(userinfo);
+            }
+        }
+        return listUser;
+    }
+    private List findUser(User user){
+        List listUser = new ArrayList<User>();
+        for(User userinfo: userList){
+            if((user.getName().equals(userinfo.getName())) && (user.getAddr().equals(userinfo.getAddr()))){
+                listUser.add(userinfo);
+            }
+        }
+        log.info("select---:{}", listUser);
+        return listUser;
+    }
+
     @RequestMapping(value = "/add")
     public ResponVo add(@RequestBody User user){
         user.setId(id++);
@@ -42,8 +67,10 @@ public class IndexController {
     @RequestMapping(value = "/select")
     public ResponVo select(@RequestBody User user){
         log.info("user:{}", user);
-        List<User> list = findUser(user);
-        log.info("user:{}", list);
+        List<User> list;
+        list = filterUserByName(userList, user.getName());
+        list = filterUserByAddr(list, user.getAddr());
+        log.info("userList:{}", list);
         return ResponVo.success(list);
     }
 }
